8de258294ad3b0a32c9551be7ddc19b63dfb0657
Justin Albrecht <jalbrec1@umd.edu>
2020-11-21 21:10:45 -0500

    Implemented logic for basic obstacle avoidance

--- include/TurtlebotWalker.h
+++ include/TurtlebotWalker.h
@@ -1,9 +1,13 @@
 class TurtlebotWalker {
   public:
-    TurtlebotWalker();
-    void drive(double speed);
+    TurtlebotWalker(double ds, double ts);
+    void drive();
     void stop();
+    void turn();
+    void rotateUntilClear();
+    void navigate();
     bool near_obstacle;
+    bool front_clear;
 
   private:
     void lidarCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
@@ -13,5 +17,7 @@ class TurtlebotWalker {
     ros::Subscriber scan_sub;
     geometry_msgs::Twist vel;
 
-    double threshold;
+    double obs_threshold;
+    double drive_speed;
+    double turn_speed;
 };
--- src/walker.cpp
+++ src/walker.cpp
@@ -1,57 +1,93 @@
+#include <vector>
 #include "ros/ros.h"
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/LaserScan.h>
 #include "../include/TurtlebotWalker.h"
 
-TurtlebotWalker::TurtlebotWalker() {
+TurtlebotWalker::TurtlebotWalker(double ds, double ts) {
     vel_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1, true);
     scan_sub = nh.subscribe<sensor_msgs::LaserScan>("/scan", 10, &TurtlebotWalker::lidarCallback, this);
     near_obstacle = false;
-    threshold = 0.2;
+    front_clear = true;
+    obs_threshold = 0.4;
+    drive_speed = ds;
+    turn_speed = ts;
 }
 
 void TurtlebotWalker::lidarCallback(const sensor_msgs::LaserScan::ConstPtr& msg) {
-    double front = msg->ranges[0];
-    if (front > msg->range_min && front < msg->range_max){
-        if (front < threshold){
+    int cone_angle = 45;
+    std::vector<double> front_cone;
+    std::vector<double> b;
+
+    b = std::vector<double>(msg->ranges.begin(),msg->ranges.begin()+cone_angle);
+    front_cone = std::vector<double>(msg->ranges.end()-cone_angle,msg->ranges.end());
+
+    front_cone.insert(front_cone.end(),b.begin(),b.end());
+
+    front_clear = true;
+
+    for (auto& range : front_cone) {
+        if (range < obs_threshold) {
             near_obstacle = true;
+            front_clear = false;
         }
-    } 
+    }
 }
 
-void TurtlebotWalker::drive(double speed) {
+void TurtlebotWalker::drive() {
     geometry_msgs::Twist vel;
-    vel.linear.x = speed;
+    vel.linear.x = drive_speed;
+
+    vel_pub.publish(vel);
+}
+
+
+void TurtlebotWalker::turn() {
+    geometry_msgs::Twist vel;
+    vel.angular.z = turn_speed;
 
     vel_pub.publish(vel);
 }
 
 void TurtlebotWalker::stop() {
+
     geometry_msgs::Twist vel;
     vel.linear.x = 0;
     vel.angular.z = 0;
 
     vel_pub.publish(vel);
+    ros::Duration(0.5).sleep();
 }
 
+void TurtlebotWalker::rotateUntilClear(){
+    turn();
+    ros::Rate rate(10);
+    while (!front_clear){
+        rate.sleep();
+        ros::spinOnce();
+    }
+    stop();
+    near_obstacle = false;
+}
+
+void TurtlebotWalker::navigate() {
+    while (ros::ok()){
+        drive();
+
+        if (near_obstacle){
+            stop();
+            rotateUntilClear();
+        }
+
+        ros::spinOnce();
+    }
+
+}
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "walker");
 
-  TurtlebotWalker walker;
+  TurtlebotWalker walker(0.2,0.2);
 
-  ros::Rate loop_rate(1);
-
-  walker.drive(.1);
-
-  while (ros::ok()){
-
-    if (walker.near_obstacle){
-        ROS_WARN_STREAM("Got too close to an obstacle. Stopping");
-        walker.stop();
-        break;
-    }
-
-    ros::spinOnce();
-  }
+  walker.navigate();
 }
